085cfaa5da4372ed79c248adfe2f34fa69cd2586
Dāgs <dagsagrinbergs@gmail.com>
2021-02-05 00:33:31 +0200

    read ints from file into array

---
+++ 01/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+func ReadInts(r io.Reader) ([]int, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(bufio.ScanWords)
+	var result []int
+	for scanner.Scan() {
+		x, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return result, err
+		}
+		result = append(result, x)
+	}
+	return result, scanner.Err()
+}
+
+func main() {
+	fptr := flag.String("fpath", "example", "read input file")
+	flag.Parse()
+
+	f, err := os.Open(*fptr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err = f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	ints, err := ReadInts(f)
+	fmt.Println(ints, err)
+}

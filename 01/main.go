53562c1a86fb29ae3a0203bb1177c6816c06e8da
Dāgs <dagsagrinbergs@gmail.com>
2021-02-05 01:31:41 +0200

    fix bug in 01a and finish naive 01b

--- 01/main.go
+++ 01/main.go
@@ -26,7 +26,7 @@ func ReadInts(r io.Reader) ([]int, error) {
 
 func FindTwo(ints []int) int {
 	for iIndex, i := range ints {
-		for jIndex := iIndex; jIndex < len(ints); jIndex++ {
+		for jIndex := iIndex + 1; jIndex < len(ints); jIndex++ {
 			if j := ints[jIndex]; i+j == 2020 {
 				return i * j
 			}
@@ -35,8 +35,23 @@ func FindTwo(ints []int) int {
 	return 0
 }
 
+func FindThree(ints []int) int {
+	for iIndex, i := range ints {
+		for jIndex := iIndex + 1; jIndex < len(ints); jIndex++ {
+			for kIndex := jIndex + 1; kIndex < len(ints); kIndex++ {
+				j := ints[jIndex]
+				k := ints[kIndex]
+				if i+j+k == 2020 {
+					return i * j * k
+				}
+			}
+		}
+	}
+	return 0
+}
+
 func main() {
-	fptr := flag.String("fpath", "example", "read input file")
+	fptr := flag.String("fpath", "input", "read input file")
 	flag.Parse()
 
 	f, err := os.Open(*fptr)
@@ -51,4 +66,5 @@ func main() {
 
 	ints, err := ReadInts(f)
 	fmt.Println(FindTwo(ints))
+	fmt.Println(FindThree(ints))
 }

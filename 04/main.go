26fde4d6fa6e6c7af8da082515eb75c01a713e64
Dāgs <dagsagrinbergs@gmail.com>
2021-02-05 20:23:19 +0200

    finish 04b

--- 04/main.go
+++ 04/main.go
@@ -35,28 +35,45 @@ func passportHasRequiredFields(passport map[string]string) bool {
 	return true
 }
 
+func isAlphanumeric(s string) bool {
+	match, _ := regexp.MatchString("^[0-9]+$", s)
+	return match
+}
+
 func passportFieldsValid(passport map[string]string) bool {
 	// Takes for granted that all required fields have been checked before
+	if !isAlphanumeric(passport["byr"]) {
+		return false
+	}
 	byr, _ := strconv.Atoi(passport["byr"])
 	if byr < 1920 || byr > 2002 {
 		return false
 	}
 
+	if !isAlphanumeric(passport["iyr"]) {
+		return false
+	}
 	iyr, _ := strconv.Atoi(passport["iyr"])
 	if iyr < 2010 || iyr > 2020 {
 		return false
 	}
 
+	if !isAlphanumeric(passport["eyr"]) {
+		return false
+	}
 	eyr, _ := strconv.Atoi(passport["eyr"])
 	if eyr < 2020 || eyr > 2030 {
 		return false
 	}
 
 	hgt := passport["hgt"]
-	hgtVal, _ := strconv.Atoi(hgt[:len(hgt)-1])
-	hgtUnit := hgt[len(hgt)-1:]
+	hgtVal, _ := strconv.Atoi(hgt[:len(hgt)-2])
+	hgtUnit := hgt[len(hgt)-2:]
 	if (hgtUnit == "cm" && (hgtVal < 150 || hgtVal > 193)) ||
 		(hgtUnit == "in" && (hgtVal < 59 || hgtVal > 76)) {
+		return false
+	}
+	if hgtUnit != "cm" && hgtUnit != "in" {
 		return false
 	}
 
@@ -91,8 +108,7 @@ func passportFieldsValid(passport map[string]string) bool {
 	if len(pid) != 9 {
 		return false
 	}
-	match, _ = regexp.MatchString("^[0-9]+$", pid)
-	if !match {
+	if !isAlphanumeric(pid) {
 		return false
 	}
 

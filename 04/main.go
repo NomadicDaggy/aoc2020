c061e0e8d00788058828ba227e414f17ec1fd3e5
Dāgs <dagsagrinbergs@gmail.com>
2021-02-05 20:12:22 +0200

    almost finish 04b - result too high

--- 04/main.go
+++ 04/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -22,7 +24,7 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
-func isPassportValid(passport map[string]string) bool {
+func passportHasRequiredFields(passport map[string]string) bool {
 	requiredKeys := [7]string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 	for _, key := range requiredKeys {
 		_, ok := passport[key]
@@ -30,6 +32,70 @@ func isPassportValid(passport map[string]string) bool {
 			return false
 		}
 	}
+	return true
+}
+
+func passportFieldsValid(passport map[string]string) bool {
+	// Takes for granted that all required fields have been checked before
+	byr, _ := strconv.Atoi(passport["byr"])
+	if byr < 1920 || byr > 2002 {
+		return false
+	}
+
+	iyr, _ := strconv.Atoi(passport["iyr"])
+	if iyr < 2010 || iyr > 2020 {
+		return false
+	}
+
+	eyr, _ := strconv.Atoi(passport["eyr"])
+	if eyr < 2020 || eyr > 2030 {
+		return false
+	}
+
+	hgt := passport["hgt"]
+	hgtVal, _ := strconv.Atoi(hgt[:len(hgt)-1])
+	hgtUnit := hgt[len(hgt)-1:]
+	if (hgtUnit == "cm" && (hgtVal < 150 || hgtVal > 193)) ||
+		(hgtUnit == "in" && (hgtVal < 59 || hgtVal > 76)) {
+		return false
+	}
+
+	hcl := passport["hcl"]
+	if hcl[0] != []byte("#")[0] {
+		return false
+	}
+	if len(hcl) != 7 {
+		return false
+	}
+	match, _ := regexp.MatchString("^[a-z0-9]*$", hcl[1:])
+	if !match {
+		return false
+	}
+
+	ecl := passport["ecl"]
+	allowedEyeColors := map[string]bool{
+		"amb": true,
+		"blu": true,
+		"brn": true,
+		"gry": true,
+		"grn": true,
+		"hzl": true,
+		"oth": true,
+	}
+	_, ok := allowedEyeColors[ecl]
+	if !ok {
+		return false
+	}
+
+	pid := passport["pid"]
+	if len(pid) != 9 {
+		return false
+	}
+	match, _ = regexp.MatchString("^[0-9]+$", pid)
+	if !match {
+		return false
+	}
+
 	return true
 }
 
@@ -67,7 +133,7 @@ func main() {
 
 	validPassportCount := 0
 	for _, passport := range passports {
-		if isPassportValid(passport) {
+		if passportHasRequiredFields(passport) && passportFieldsValid(passport) {
 			validPassportCount++
 		}
 	}

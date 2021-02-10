e8a7b9b7ea6f8de9237cb3e317c8819dd9e6b683
Dāgs <dagsagrinbergs@gmail.com>
2021-02-10 22:17:55 +0200

    finish 08b

--- 08/main.go
+++ 08/main.go
@@ -51,15 +51,59 @@ func processInstruction(
 
 func main() {
 	lines, _ := readLines("input")
-	visitedLines := make(map[int]bool)
+	accBeforeInstructionAt := make(map[int]int)
 	acc := 0
 	i := 0
+	var instructionOrder []int // needed for 08b
+	// determine failure point (08a)
 	for {
-		if lineAlreadyVisited, _ := visitedLines[i]; lineAlreadyVisited {
-			fmt.Println(acc)
-			return
+		instructionOrder = append(instructionOrder, i)
+		if _, ok := accBeforeInstructionAt[i]; ok {
+			fmt.Println(acc, " :failure point")
+			accBeforeInstructionAt[i] = acc
+			break
 		}
-		visitedLines[i] = true
+		accBeforeInstructionAt[i] = acc
 		i, acc = processInstruction(lines[i], acc, i)
+	}
+	// determine broken instruction (08b)
+	// holds line indexes from which there is no path out of the boot code
+	// blacklist := make(map[int]bool)
+
+	// Search backwards through original path
+	endIndex := len(lines)
+	for j := len(instructionOrder) - 1; j > 0; j-- {
+		// Change first changeable instruction and look for ending
+		lineIndex := instructionOrder[j]
+		instructionString := lines[lineIndex]
+		switch instructionString[:3] {
+		case "nop":
+			instructionString = "jmp" + instructionString[3:]
+		case "jmp":
+			instructionString = "nop" + instructionString[3:]
+		default:
+			continue
+		}
+
+		// run with swapped instruction
+		i = lineIndex
+		acc, ok := accBeforeInstructionAt[lineIndex]
+		if !ok {
+			panic("malformed history")
+		}
+		i, acc = processInstruction(instructionString, acc, i)
+
+		// continue until end found or infinite loop
+		linesVisited := make(map[int]bool)
+		for {
+			if i == endIndex {
+				fmt.Println(acc)
+				return
+			} else if _, ok := linesVisited[i]; ok {
+				break
+			}
+			linesVisited[i] = true
+			i, acc = processInstruction(lines[i], acc, i)
+		}
 	}
 }
